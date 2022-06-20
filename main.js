0a454089d9066c2cb156186032dea00a02cfc72a
Igor S <is@embact.io>
2022-06-20 21:46:04 +0300

    1

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const winHeight = window.innerHeight;
-const mainBlock = document.querySelector('.panelCon');
+const mainBlock = document.querySelector(".panelCon");
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.body.style.height = (mainBlock.scrollHeight) + 'px';
-    console.log(mainBlock.scrollHeight)
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.style.height = mainBlock.scrollHeight + "px";
+    console.log(mainBlock.scrollHeight);
 });
 
-window.addEventListener('resize', function (event) {
-    mainBlock.style.height = window.innerHeight; + 'px';
+window.addEventListener("resize", function (event) {
+    mainBlock.style.height = window.innerHeight;
+    +"px";
 });
 
-window.addEventListener('scroll', () => {
-    mainBlock.style.bottom = window.pageYOffset * -1 + 'px'
+window.addEventListener("scroll", () => {
+    mainBlock.style.bottom = window.pageYOffset * -1 + "px";
 });
